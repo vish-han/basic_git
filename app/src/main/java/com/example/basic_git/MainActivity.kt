3f966df4fe3139e5e9d3b6e8c29e59c268cd7327
Vishal <vshlchauhab@gmail.com>
2020-09-05 10:10:17 +0530

    first commit

---
+++ app/src/main/java/com/example/basic_git/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.basic_git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        print("my name is vishal")
+    }
+}
