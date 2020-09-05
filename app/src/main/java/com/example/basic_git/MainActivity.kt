f1c069240debfdaee519a1f8b71db8beb063e36a
Vishal <vshlchauhab@gmail.com>
2020-09-05 13:16:21 +0530

    third commit

--- app/src/main/java/com/example/basic_git/MainActivity.kt
+++ app/src/main/java/com/example/basic_git/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 println("my second change")
+        println("my third commit")
     }
 }
